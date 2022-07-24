d1d812c337f12644f97c35e8038e7d79341cd9e5
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2022-07-24 14:59:52 +0300

    Link to libtensorflow_framework.so.2
    Correct dependency which does not require Python

--- plugin/build.rs
+++ plugin/build.rs
@@ -4,10 +4,10 @@ fn main() {
     // TODO: support other versions aside from 3.10
     println!(
         "cargo:rustc-link-search={}",
-        PathBuf::from("./venv/lib/python3.10/site-packages/tensorflow/python/")
+        PathBuf::from("./venv/lib/python3.10/site-packages/tensorflow/")
             .to_str()
             .unwrap()
     );
     // TODO: support other OSs, not only Linux
-    println!("cargo:rustc-link-arg=-l:_pywrap_tensorflow_internal.so");
+    println!("cargo:rustc-link-arg=-l:libtensorflow_framework.so.2");
 }
