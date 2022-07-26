1965ad90271cf1766170c6465c8c9afe1904a146
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2022-07-26 11:43:13 +0300

    Remove extra includes
    Reduce build time and binding size

--- bindings/wrapper.hh
+++ bindings/wrapper.hh
@@ -1,9 +1,8 @@
-#include "tensorflow/c/experimental/grappler/grappler.h"
 #include "tensorflow/c/experimental/stream_executor/stream_executor.h"
-#include "tensorflow/c/tf_status.h"
 #include "tensorflow/c/kernels.h"
-#include "tensorflow/c/ops.h"
-#include "tensorflow/c/tf_datatype.h"
+
+// Needed for optimizer only
+// #include "tensorflow/c/experimental/grappler/grappler.h"
 
 struct SP_Stream_st {
   explicit SP_Stream_st(void* stream_h) : stream_handle(stream_h) {}
